e74809abb64f472df7e3769cebeaa65ebac1d307
AyuHamano <aline_soma@discente.ufg.br>
2023-07-23 18:01:01 -0300

    configurações iniciais do canvas

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const sprites = new Image();
+sprites.src = "./assets/sprites.png"
+
+const canvas = document.querySelector('canvas')
+
+const context = canvas.getContext('2d')
+
+context.drawImage( 
+    
+    
+)
