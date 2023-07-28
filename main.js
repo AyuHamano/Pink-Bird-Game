ebaf1f04ae1f11832a028b6df6c662b79be45494
Ayumi Hamano <98706488+AyuHamano@users.noreply.github.com>
2023-07-28 17:19:09 -0300

    criação do Pink e do chão do cenário para inserir no canvas

--- main.js
+++ main.js
@@ -1,12 +1,72 @@
 
-const sprites = new Image();
-sprites.src = "./assets/sprites.png"
+const source = new Image();
+source.src = './source.png'
 
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('canvas');
+console.log(document.querySelector('canvas'));
 
 const context = canvas.getContext('2d')
 
-context.drawImage( 
-    
-    
-)
+const pinkBird = {
+    sourceX: 0,
+    sourceY: 0,
+    largura: 33,
+    altura: 24,
+    positionX: 30,
+    positionY: 50,
+    autoDesenha() {
+        context.drawImage(
+            source,
+            pinkBird.sourceX,
+            pinkBird.sourceY,
+            pinkBird.largura,
+            pinkBird.altura,
+            pinkBird.positionX,
+            pinkBird.positionY,
+            pinkBird.largura,
+            pinkBird.altura,
+        ) 
+    }
+}
+
+const piso = {
+    sourceX: 0,
+    sourceY: 610,
+    largura: 224,
+    altura: 112,
+    positionX: 0,
+    positionY: canvas.height - 112,
+    autoDesenha() {
+        context.drawImage(
+            source,
+            piso.sourceX,
+            piso.sourceY,
+            piso.largura,
+            piso.altura,
+            piso.positionX,
+            piso.positionY,
+            piso.largura,
+            piso.altura,
+        ) 
+        context.drawImage(
+            source,
+            piso.sourceX,
+            piso.sourceY,
+            piso.largura,
+            piso.altura,
+            piso.positionX + piso.largura,
+            piso.positionY,
+            piso.largura,
+            piso.altura,
+        ) 
+    }
+}
+
+
+function loop() {
+    pinkBird.autoDesenha()
+    piso.autoDesenha()
+    requestAnimationFrame(loop)
+}
+
+loop()
